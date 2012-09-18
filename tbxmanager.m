9eb217d7faaed35574eaacd880e36fe69e9e52e3
Michal Kvasnica <kvasnica@users.noreply.github.com>
2012-09-18 10:47:25 +0200

    automatically add tbxmanager directory to matlab path
    But only for the current session.

--- tbxmanager.m
+++ tbxmanager.m
@@ -43,6 +43,9 @@ function tbxmanager(command, varargin)
 %     Boston, MA  02111-1307  USA
 % ------------------------------------------------------------------------
 
+%% add self to path
+addpath(fileparts(which(mfilename)));
+
 if nargin==0
 	help(mfilename);
 	return
